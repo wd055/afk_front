be90c80a08de352c0db5dc0275b16fec756ba536
Vlasov Denis <wd055@mail.ru>
2020-04-20 23:57:50 +0300

    Init relize Vk mini apps

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -6,30 +6,50 @@ import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Input from '@vkontakte/vkui/dist/components/Input/Input';
+import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
+import TabsItem from '@vkontakte/vkui/dist/components/TabsItem/TabsItem';
+import Tabs from '@vkontakte/vkui/dist/components/Tabs/Tabs';
+import Separator from '@vkontakte/vkui/dist/components/Separator/Separator';
+import Header from '@vkontakte/vkui/dist/components/Header/Header';
+import FormLayoutGroup from '@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup';
+
+import background_img from '../img/GZ.png';
+import './Map.css';
 
 const output_graf = draw_graf();
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
+		<PanelHeader>Навигатор по МГТУ</PanelHeader>
 
-		<Group title="Navigation Example">
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
+		<Group title="Карта" style={{margin:25}}>
+			<Div id="background_map" className="contain-map">
+				<img id="background_img" className="map" src={background_img} alt="Фон карты не прогрузился"/>
+				{/* <img id="background_img" src={background_img} alt="Фон карты не прогрузился" width={Math.min(document.documentElement.clientWidth, 1059)}  width="350" /> */}
 			</Div>
-			
-			<Div id="background_map">
-				<img id="background_img" src="../img/GZ.png" alt="Фон карты не прогрузился" width="100"/>
-			</Div>
-			
-			<Div id="point_map">
-				<svg id="svg_map" viewBox="0 0 1059 873" width={Math.min(document.documentElement.clientWidth, 1059)} height="873">
+
+			<Div id="point_map" className="contain-map">
+				<svg id="svg_map" className="map" viewBox="0 0 1059 873">
+				{/* <svg id="svg_map" viewBox="0 0 1059 873" width={Math.min(document.documentElement.clientWidth, 1059)}> */}
 					{output_graf}
 				</svg>
 			</Div>
 		</Group>
+
+		<FixedLayout vertical="bottom">
+			{/* <Div className="input" style={{ display: 'flex'}}> */}
+			{/* <Div className="input">
+				<Input placeholder="Откуда" style={{ marginRight: 8}}/><br></br>
+				<Input placeholder="Куда" className="input_right" />
+			</Div> */}
+			<Div>
+				<FormLayoutGroup top="Введитепвкп" bottom="Пароль может содержать только латинские буквы и цифры.">
+					<Input type="text"  placeholder="Откуда" />
+					<Input type="text" placeholder="Куда" />
+				</FormLayoutGroup>
+			</Div>
+		</FixedLayout>
 	</Panel>
 );
 
@@ -58,20 +78,20 @@ function draw_graf() {
 		text_x -= 6;
 
 		if (dots[i].floor == floor || dots[i].is_stairs == true) {
-			input_str.push(<circle id={dots[i].id}
+			input_str.push(<circle key={dots[i].id} id={dots[i].id}
 				cx={dots[i].cx} cy={dots[i].cy}
-				r="12" stroke="black" fill="red" />);
+				r="12" stroke="black" fill="red"/>);
 
-			input_str.push(<text id={dots[i].id + "_text"}
+			input_str.push(<text key={dots[i].id + "_text"} id={dots[i].id + "_text"}
 				x={dots[i].cx - text_x} y={dots[i].cy + 6}
 				fill="white">{dots[i].id}</text>);
 			for (var j in dots[i].near_dots) {
-				if (document.getElementById(`${dots[i].near_dots[j]}_${dots[i].id}`) == null && dots[dots[i].near_dots[j]].floor == floor)
-				input_str.unshift(<line 
+				// if (document.getElementById(`${dots[i].near_dots[j]}_${dots[i].id}`) == null && dots[dots[i].near_dots[j]].floor == floor)
+				input_str.unshift(<line key={dots[i].id + "_" + dots[i].near_dots[j]}
 								id = {dots[i].id + "_" + dots[i].near_dots[j]}
 								x1 = {dots[i].cx} y1 = {dots[i].cy}
 								x2 = {dots[dots[i].near_dots[j]].cx} y2 = {dots[dots[i].near_dots[j]].cy}
-								stroke = "red" stroke-width = "2" />);
+								stroke = "red"  />);
 			}
 		}
 	}
