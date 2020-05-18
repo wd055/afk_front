6bdeb5fb31cad3e9dbd45d566911df68f6a5bef2
Vlasov Denis <wd055@mail.ru>
2020-05-19 01:54:57 +0300

    Add succes and error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import View from '@vkontakte/vkui/dist/components/View/View';
 import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 
+
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Placeholder from '@vkontakte/vkui/dist/components/Placeholder/Placeholder';
+import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
+import Icon56CheckCircleOutline from '@vkontakte/icons/dist/56/check_circle_outline';
+import Icon56ErrorOutline from '@vkontakte/icons/dist/56/error_outline';
+
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+	useParams
+} from "react-router-dom";
+
 import Home from './panels/Home';
 
 const App = () => {
@@ -43,10 +60,67 @@ const App = () => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
+	const redIcon = {
+		color: 'red'
+	};
+	const blueIcon = {
+		color: 'var(--accent)'
+	};
+
 	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-		</View>
+
+		<Router>
+			{/* <div>
+				<ul>
+					<li>
+						<Link to="/"></Link>
+					</li>
+					<li>
+						<Link to="/success"></Link>
+					</li>
+					<li>
+						<Link to="/error"></Link>
+					</li>
+				</ul> */}
+
+				<Switch>
+					<Route path="/success">
+						<View activePanel="Success" popout={popout}>
+							<Panel id="Success">
+								<PanelHeader>Успешная авторизация</PanelHeader>
+								<Placeholder
+									icon={<Icon56CheckCircleOutline style={blueIcon} />}
+									stretched
+									id="Placeholder"
+								>
+									Успешная авторизация!
+							</Placeholder>
+							</Panel>
+						</View>
+					</Route>
+					<Route path="/error">
+						<View activePanel="Error" popout={popout}>
+							<Panel id="Error">
+								<PanelHeader>Ошибка авторизации</PanelHeader>
+								<Placeholder
+									icon={<Icon56ErrorOutline style={redIcon} />}
+									stretched
+									id="Placeholder"
+								>
+									Ошибка авторизации<br />Попробуйте позже или свяжитесь с администратором группы!
+							</Placeholder>
+							</Panel>
+						</View>
+					</Route>
+					<Route path="/">
+						{/* <Home /> */}
+						<View activePanel={activePanel} popout={popout}>
+							<Home id='home' fetchedUser={fetchedUser} go={go} setPopout={setPopout} />
+						</View>
+					</Route>
+				</Switch>
+			{/* </div> */}
+		</Router>
 	);
 }
 
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -12,14 +12,29 @@ import Select from '@vkontakte/vkui/dist/components/Select/Select';
 // import Textarea from '@vkontakte/vkui/dist/components/Textarea/Textarea';
 import Checkbox from '@vkontakte/vkui/dist/components/Checkbox/Checkbox';
 import Link from '@vkontakte/vkui/dist/components/Link/Link';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import Snackbar from '@vkontakte/vkui/dist/components/Snackbar/Snackbar';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import Icon16Clear from '@vkontakte/icons/dist/16/clear';
+
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import SimpleCell from '@vkontakte/vkui/dist/components/SimpleCell/SimpleCell';
+import InfoRow from '@vkontakte/vkui/dist/components/InfoRow/InfoRow';
 
 import Placeholder from '@vkontakte/vkui/dist/components/Placeholder/Placeholder';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
 import Icon56CheckCircleOutline from '@vkontakte/icons/dist/56/check_circle_outline';
+import Icon56ErrorOutline from '@vkontakte/icons/dist/56/error_outline';
 
 import bridge from '@vkontakte/vk-bridge';
 
-const App = ({ id, fetchedUser, go }) => {
+const App = ({ id, fetchedUser, go, setPopout }) => {
+	const redIcon = {
+		color: 'red'
+	};
+	const blueIcon = {
+		color: 'var(--accent)'
+	};
 
 	var params = window
 		.location
@@ -36,10 +51,22 @@ const App = ({ id, fetchedUser, go }) => {
 		);
 
 	const [categories, setCategories] = useState([]);
+	const [getCategories, setGetCategories] = useState([]);
+	const [showForm, setShowForm] = useState(true);
+	const [submitSuccess, setSubmitSuccess] = useState("Успешно!");
+	const [snackbar, setSnackbar] = useState();
+
+	const [email, setEmail] = useState();
+	const [phone, setPhone] = useState();
+	const [payments_edu, setPayments_edu] = useState();
+	const [name, setName] = useState();
+
+
 	useEffect(() => {
 		var url = "http://thingworx.asuscomm.com/profkom_bot/all_categories/";
-		var url = "http://localhost:8000/profkom_bot/all_categories/";
-		if (categories.length == 0)
+		// var url = "http://localhost:8000/profkom_bot/all_categories/";
+		var temp = true
+		if (categories.length == 0 && showForm == true) {
 			fetch(url, {
 				method: 'POST',
 				headers: {
@@ -47,26 +74,52 @@ const App = ({ id, fetchedUser, go }) => {
 				}
 			})
 				.then(response => response.json())
-				.then(data => setCategories(data))
+				.then((data) => {
+					setCategories(data)
+				},
+					(error) => {
+						setShowForm(false)
+						setSubmitSuccess(<div>Ошибка подключения<br />Пожалуйста, попробуйте через несколько минут!</div>)
+						console.error('get category:', error)
+					})
+
+
+
+			var url = "http://thingworx.asuscomm.com/profkom_bot/get_form/";
+			// var url = "http://localhost:8000/profkom_bot/get_form/";
+
+			var data = {
+				token: params['token'],
+			}
+
+			fetch(url, {
+				method: 'POST',
+				body: JSON.stringify(data),
+				headers: {
+					'Origin': 'http://localhost:10888/'
+				}
+			})
+				.then(response => response.json())
+				.then((data) => {
+					if (data != "Error"){
+						console.log(JSON.parse(data));
+						data = JSON.parse(data);
+						setName(data.name)
+						setEmail(data.email)
+						setPhone(data.phone)
+						setGetCategories(data.categories)
+					}else{
+						setShowForm(false)
+						setSubmitSuccess(<div>Ошибка авторизации</div>)
+					}
+				},
+					(error) => {
+						setShowForm(false)
+						setSubmitSuccess(<div>Ошибка подключения<br />Пожалуйста, попробуйте через несколько минут!</div>)
+						console.error('get category:', error)
+					})
+		}
 	});
-
-	const [email, setEmail] = useState();
-	const [phone, setPhone] = useState();
-	const [payments_edu, setPayments_edu] = useState();
-
-
-	function onChangeEmail(e) {
-		const { name, value } = e.currentTarget;
-		setEmail(value);
-	}
-	function onChangePhone(e) {
-		const { name, value } = e.currentTarget;
-		setPhone(value);
-	}
-	function onChangePayments_edu(e) {
-		const { name, value } = e.currentTarget;
-		setPayments_edu(value);
-	}
 
 	var clickEmail = true;
 	var clickPhone = true;
@@ -85,9 +138,8 @@ const App = ({ id, fetchedUser, go }) => {
 
 	function onFormClick(e) {
 		if (!email || !phone || !payments_edu)
-			return
-		document.getElementsByName("category")
-		var categorys = document.getElementsByName("category")//document.forms[0].elements;
+			return;
+		var categorys = document.getElementsByName("category");
 		var data = {
 			token: params['token'],
 			email: document.getElementById("email").value,
@@ -98,12 +150,14 @@ const App = ({ id, fetchedUser, go }) => {
 
 		for (var i = 0; i < categorys.length; i++) {
 			if (categorys[i].checked) {
-				data.categories.push(categories[i])
+				data.categories.push(categories[i]);
 			}
 		}
 
 		var url = "http://thingworx.asuscomm.com/profkom_bot/form/";
-		var url = "http://localhost:8000/profkom_bot/form/";
+		// var url = "http://localhost:8000/profkom_bot/form/";
+		setPopout(<ScreenSpinner size='large' />);
+
 		fetch(url, {
 			method: 'POST',
 			body: JSON.stringify(data),
@@ -112,83 +166,114 @@ const App = ({ id, fetchedUser, go }) => {
 			}
 		})
 			.then(response => response.json())
-			.then(data => {
-				if (data == 'Success')
-					window.close();
-			})
+			.then((data) => {
+				setPopout(null);
+				setShowForm(false);
+				if (data != 'Success') {
+					setSubmitSuccess(<div>Ошибка отправики <br />Попробуйте еще раз или свяжитесь с администратором!</div>);
+				}
+			},
+				(error) => {
+					setSnackbar(<Snackbar
+						layout="vertical"
+						onClose={() => setSnackbar(null)}
+						before={<Avatar size={24} style={redIcon}><Icon16Clear fill="#fff" width={14} height={14} /></Avatar>}
+					>
+						Ошибка соединения
+					  </Snackbar>);
+					setPopout(null);
+					console.error('send form:', error);
+				})
 	};
 	// console.log('token', params['token'])
 
-	const qwe =
+	const Home =
 		<Panel id={id}>
 			<PanelHeader>Форма</PanelHeader>
-			<FormLayout>
-				<Input
-					type="email"
-					top="E-mail"
-					name="email"
-					id="email"
-					onClick={onEmailClick}
-					onChange={onChangeEmail}
-					value={email}
-					status={email ? 'valid' : 'error'}
-					bottom={email ? '' : 'Пожалуйста, введите электронную почту'}
-					required
-				/>
-				<Input
-					id="phone"
-					type="phone"
-					top=" Телефон"
-					name="phone"
-					onClick={onPhoneClick}
-					onChange={onChangePhone}
-					value={phone}
-					status={phone ? 'valid' : 'error'}
-					bottom={phone ? '' : 'Пожалуйста, введите номер телефона'}
-					required
-				/>
+			{showForm
+				? <Group>
+					<Group>
+						<SimpleCell>
+							<InfoRow header="ФИО">
+								{name}
+							</InfoRow>
+						</SimpleCell>
+					</Group>
+					<FormLayout>
+						<Input
+							type="email"
+							top="E-mail"
+							name="email"
+							id="email"
+							onClick={onEmailClick}
+							onChange={(e) => {
+								const { value } = e.currentTarget;
+								setEmail(value);
+							}}
+							value={email}
+							status={email ? 'valid' : 'error'}
+							bottom={email ? '' : 'Пожалуйста, введите электронную почту'}
+							required
+						/>
+						<Input
+							id="phone"
+							type="phone"
+							top=" Телефон"
+							name="phone"
+							onClick={onPhoneClick}
+							onChange={(e) => {
+								const { value } = e.currentTarget;
+								setPhone(value);
+							}}
+							value={phone}
+							status={phone ? 'valid' : 'error'}
+							bottom={phone ? '' : 'Пожалуйста, введите номер телефона'}
+							required
+						/>
 
-				<Select
-					top="Форма обучения"
-					placeholder="Выберите форму обучения"
-					id='payments_edu'
-					name="payments_edu"
-					onChange={onChangePayments_edu}
-					value={payments_edu}
-					status={payments_edu ? 'valid' : 'error'}
-					bottom={payments_edu ? '' : 'Пожалуйста, выберите форму обучения'}
-					required
+						<Select
+							top="Форма обучения"
+							placeholder="Выберите форму обучения"
+							id='payments_edu'
+							name="payments_edu"
+							onChange={(e) => {
+								const { value } = e.currentTarget;
+								setPayments_edu(value);
+							}}
+							value={payments_edu}
+							status={payments_edu ? 'valid' : 'error'}
+							bottom={payments_edu ? '' : 'Пожалуйста, выберите форму обучения'}
+							required
+						>
+							<option value="free">Бюджетная</option>
+							<option value="paid">Платная</option>
+						</Select>
+
+						<FormLayoutGroup top="Выберите подходящие категории:">
+							{/* <Radio name="type">Паспорт</Radio>
+				<Radio name="type">Загран</Radio> */}
+							{categories.map((category, i) => (
+								<Checkbox name="category" id={i.toString()} checked={getCategories.indexOf(category) != -1}>{category}</Checkbox>
+							))}
+						</FormLayoutGroup>
+						{/* <Checkbox>Согласен со всем <Link>этим</Link></Checkbox> */}
+
+						{/* <Textarea top="О себе" /> */}
+						<Button size="xl" onClick={onFormClick}>Зарегистрироваться</Button>
+					</FormLayout>
+				</Group>
+				: <Placeholder
+					icon={submitSuccess == 'Успешно!' ? <Icon56CheckCircleOutline style={blueIcon}/> : <Icon56ErrorOutline style={redIcon} />}
+					// action={<Button size="l" mode="tertiary">Показать все сообщения</Button>}
+					stretched
+					id="Placeholder"
+					style="visibility:hidden;"
 				>
-					<option value="free">Бесплатное</option>
-					<option value="paid">Платное</option>
-				</Select>
-
-				<FormLayoutGroup top="Выберите подходящие категории:">
-					{/* <Radio name="type">Паспорт</Radio>
-					<Radio name="type">Загран</Radio> */}
-					{categories.map((category, i) => (
-						<Checkbox name="category" id={i.toString()}>{category}</Checkbox>
-					))}
-				</FormLayoutGroup>
-				{/* <Checkbox>Согласен со всем <Link>этим</Link></Checkbox> */}
-
-				{/* <Textarea top="О себе" /> */}
-				<Button size="xl" onClick={onFormClick}>Зарегистрироваться</Button>
-			</FormLayout>
-
-			<PanelHeader
-				left={<PanelHeaderBack onClick={this.onNavClick} data-to="example-1" />}
-			>Плейсхолдеры</PanelHeader>
-
-			<Placeholder
-				icon={<Icon56CheckCircleOutline />}
-				action={<Button size="l" mode="tertiary">Показать все сообщения</Button>}
-				stretched
-			>
-				Нет непрочитанных<br />сообщений
-          </Placeholder>
+					{submitSuccess}
+				</Placeholder>}
+			{snackbar}
 		</Panel>
-	return qwe;
+	return Home;
 }
 
 export default App;
